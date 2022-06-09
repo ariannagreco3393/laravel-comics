1d35747620bf8df23895e99222148a2e901672e5
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-09 18:03:42 +0200

    add header and footer partials add img add db php

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layout.app')
+
+@section('content')
+<div class="hero_image"></div>
+<section class="comics">
+    <div class="container py-5">
+        <div class="title">
+            <h2>current series</h2>
+        </div>
+        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6">
+            @forelse($comics as $comic)
+            <div class="col">
+                <div class="comic">
+                    <div class="img">
+                        <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}" />
+                    </div>
+                    <div class="text">
+                        <small>{{$comic['series']}}</small>
+                    </div>
+                </div>
+            </div>
+            @empty
+            <p>Nothing to show</p>
+            @endforelse
+        </div>
+        <div class="load_more">
+            <a href="#" class="btn-primary">load more</a>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ laravel/resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="top">
+        <div class="container text-uppercase">
+            <div class="row justify-content-end">
+                <div class="col-2">
+                    <small>dc power&trade; visa&reg;</small>
+                </div>
+                <div class="col-2">
+                    <small>additional dc sites <i class="fas fa-caret-down"></i></small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-2 flex align-items-center">
+                    <img width="80" src="{{asset('../img/dc-logo.png')}}" alt="logo DC" />
+                </div>
+                <div class="col-8">
+                    <ul>
+                        <li><a href="#">characters</a></li>
+                        <li><a href="#">comics</a></li>
+                        <li><a href="#">movies</a></li>
+                        <li><a href="#">tv</a></li>
+                        <li><a href="#">games</a></li>
+                        <li><a href="#">collectibles</a></li>
+                        <li><a href="#">videos</a></li>
+                        <li><a href="#">fans</a></li>
+                        <li><a href="#">news</a></li>
+                        <li><a href="#">shop <i class="fa-solid fa-caret-down"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-2">
+                    <div class="search d-flex align-items-center justify-content-end ms-auto">
+                        <input type="text" name="search" id="search" placeholder="Search">
+                        <i class="fas fa-magnifying-glass"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</header>
