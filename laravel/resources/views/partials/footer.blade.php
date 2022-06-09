fad0dbaea6bee09d6686f48a397e4d87564b0bcf
ariannagreco3393 <a.greco3393@gmail.com>
2022-06-09 19:52:53 +0200

    add style css

--- laravel/resources/views/partials/footer.blade.php
+++ laravel/resources/views/partials/footer.blade.php
@@ -1,3 +1,86 @@
 <footer>
-    This is footer
+    <div class="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 py-5">
+                    <div class="row">
+                        <div class="col-3">
+                            <h5>DC COMICS</h5>
+                            <ul>
+                                <li><small>Characters</small></li>
+                                <li><small>Comics</small></li>
+                                <li><small>Movies</small></li>
+                                <li><small>TV</small></li>
+                                <li><small>Games</small></li>
+                                <li><small>Videos</small></li>
+                                <li><small>News</small></li>
+                            </ul>
+                            <h5>Shop</h5>
+                            <ul>
+                                <li><small>Shop DC</small></li>
+                                <li><small>Shop DC Collectibles</small></li>
+                            </ul>
+                        </div>
+                        <div class="col-3">
+                            <h5>DC</h5>
+                            <ul>
+                                <li><small>Terms of Use</small></li>
+                                <li><small>Privacy policy (New)</small></li>
+                                <li><small>Ad Choise</small></li>
+                                <li><small>Advertising</small></li>
+                                <li><small>Jobs</small></li>
+                                <li><small>Subscriptions</small></li>
+                                <li><small>Talent Workshop</small></li>
+                                <li><small>CPSC Certificates</small></li>
+                                <li><small>Ratings</small></li>
+                                <li><small>Show Help</small></li>
+                                <li><small>Contact Us</small></li>
+                            </ul>
+                        </div>
+                        <div class="col-3">
+                            <h5>SITES</h5>
+                            <ul>
+                                <li><small>DC</small></li>
+                                <li><small>MAD Magazine</small></li>
+                                <li><small>DC Kids</small></li>
+                                <li><small>DC Universe</small></li>
+                                <li><small>DC Power Visa</small></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="cookies">
+                        <small>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved. <br><a href="#">Cookies Settings</a></small>
+                    </div>
+                </div>
+                <div class="col-6 ms-auto">
+                    <div class="row h-100">
+                        <div class="col-11 bg"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="sign_up">
+                        <button>sign-up now!</button>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="follow h-100">
+                        <h5>follow us</h5>
+                        <div class="icon">
+                            <img src="{{asset('../img/footer-facebook.png')}}" alt="Facebook" />
+                            <img src="{{asset('../img/footer-twitter.png')}}" alt="Twitter" />
+                            <img src="{{asset('../img/footer-youtube.png')}}" alt="Youtube" />
+                            <img src="{{asset('../img/footer-pinterest.png')}}" alt="Pinterest" />
+                            <img src="{{asset('../img/footer-periscope.png')}}" alt="Periscope" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
